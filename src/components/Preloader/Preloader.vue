4fb21af82f8e5177bb4ee154b3a5502815cf4d4a
emiltukhvatullin <e.tuhvatullin@uplab.ru>
2025-04-22 23:39:32 +0500

    заменил прелоадер

--- src/components/Preloader/Preloader.vue
+++ src/components/Preloader/Preloader.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="preloader">
-    <div class="preloader__spinner" />
+    <div class="preloader__frame">
+      <div class="preloader__spinner" />
+    </div>
   </div>
 </template>
 
