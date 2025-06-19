1b2eee4cb4372efaffa057c228d6a988a0b40018
emiltukhvatullin <e.tuhvatullin@uplab.ru>
2025-06-19 20:49:51 +0500

    hotfix

--- src/layouts/Auth.vue
+++ src/layouts/Auth.vue
@@ -3,5 +3,11 @@
     <div class="auth-layout__content">
       <router-view />
     </div>
+
+    <Cookie />
   </div>
 </template>
+
+<script setup>
+  import Cookie from '@/components/cookie/Cookie.vue';
+</script>
